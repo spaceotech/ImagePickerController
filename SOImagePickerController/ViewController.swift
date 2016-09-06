cf19116d79fb148ce060670a555f16fd8e5535c7
hiteshtrivedi <hitesh_trivedi@spaceotechnologies.com>
2016-09-06 17:37:19 +0530

    First Commit

---
+++ SOImagePickerController/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  SOImagePickerController
+//
+//  Created by Hitesh on 9/6/16.
+//  Copyright © 2016 myCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //Action for capture image from Camera
+    @IBAction func actionClickOnCamera(sender: AnyObject) {
+        //This condition is used for check availability of camera
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true;
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagePicker.delegate = self
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }else{
+            let alert = UIAlertController(title: "Alert", message: "You don't have a camera for this device", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //Action for fetch image from Gallery
+    @IBAction func actionClickOnGallery(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true;
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imgView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+//        imgView.image = image
+//        dismissViewControllerAnimated(true, completion: nil)
+//    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
